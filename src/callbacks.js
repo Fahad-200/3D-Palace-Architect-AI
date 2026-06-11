af5fa4eb8a6c7235bc504257b1226798550b17ce
Fahad <fahadameen.brigade@gmail.com>
2026-06-11 23:37:32 +0530

    feat: add palace secrets and build date

--- src/callbacks.js
+++ src/callbacks.js
@@ -5,3 +5,7 @@ export function registerUpdateCallback(fn) {
 
 export const lodObjects = [];
 export function registerLOD(lod) { lodObjects.push(lod); }
+
+export const BUILD_DATE = '2026-06-11';
+const SECRETS = ['The ballroom chandelier has 7 lights', 'The library spiral has 44 treads', 'The basement is colder than the attic'];
+export function getPalaceSecrets() { return [...SECRETS]; }
